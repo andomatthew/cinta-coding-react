94b9a2e8d9fe4b5fb98d254cb18849a5c5c8ae9a
Alexandro M <alexandro.matthew@gmail.com>
2023-02-04 00:11:19 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { FaBeer } from 'react-icons/fa';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-2xl text-stone-600'>
+        Init project
+      </h1>
+    </div>
+  )
+}
+
+export default App
