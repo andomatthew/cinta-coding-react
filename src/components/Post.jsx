210f93a82ca32700ee57ce650fca015d0a0805a4
Alexandro M <alexandro.matthew@gmail.com>
2023-02-04 18:18:36 +0700

    feat: read data dashboard

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,31 +1,47 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import { SlBubble } from "react-icons/sl"
 
 import { useNavigate } from "react-router-dom"
 
-const Post = () => {
+const Post = ({ id, title, body, userName }) => {
   const navigate = useNavigate()
+
+  const [comments, setComments] = useState([])
 
   const handleClick = (id) => {
     navigate(`/posts/${id}`)
   }
 
+  const fetchTotalComments = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+    )
+
+    if (res.ok) {
+      const data = await res.json()
+      setComments(data)
+    }
+  }
+
+  useEffect(() => {
+    fetchTotalComments()
+  }, [])
+
   return (
     <div className="flex gap-x-4">
-      <p className="font-bold">Name</p>
+      <p className="font-bold">{userName || "Name"}</p>
       <div>
-        <p className="text-slate-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          nesciunt ut nulla eveniet nam voluptate!
-        </p>
+        <p className="text-slate-400">{title}</p>
         <div className="flex items-center mt-2 gap-x-8 cursor-pointer">
           <div
             onClick={() => handleClick("1")}
             className="flex items-center gap-x-2"
           >
             <SlBubble className="text-blue-300" />
-            <p className="text-xs font-semibold text-blue-400">5</p>
+            <p className="text-xs font-semibold text-blue-400">
+              {comments.length}
+            </p>
           </div>
           <p
             onClick={() => handleClick("2")}
--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,23 +1,106 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
 
 import Search from "../components/Search"
 import Post from "../components/Post"
 
+import { DataContext } from "../context/DataContext"
+
 const Dashboard = () => {
+  const [posts, setPosts] = useState([])
+  const [showPosts, setShowPosts] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [pagination, setPagination] = useState([])
+  const [perPage, setPerPage] = useState(1)
+  const users = useContext(DataContext)
+
+  const fetchPosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    if (res.ok) {
+      const data = await res.json()
+      setPosts(data)
+    }
+  }
+
+  const initPagination = () => {
+    let temp = []
+    let index = 0
+    while (index < perPage) {
+      temp.push(`${index + 1}`)
+      index++
+    }
+    setPagination(temp)
+  }
+
+  const setupPagination = (startIndex = 0, page = currentPage) => {
+    let temp = posts.slice(startIndex, perPage * page)
+    const result = temp.map((it) => {
+      return {
+        ...it,
+        userName: users.find((user) => user.id === it.userId).name,
+      }
+    })
+    setShowPosts(result)
+  }
+
+  const onChangePagination = (page) => {
+    setCurrentPage(page)
+    const startIndex = page > 1 ? (page - 1) * perPage + 1 : 0
+    setupPagination(startIndex, page)
+    window.scrollTo(0, 0)
+  }
+
+  const onPrev = () => {
+    if (currentPage === 1) return
+    onChangePagination(currentPage - 1)
+  }
+
+  const onNext = () => {
+    if (currentPage === 10) return
+    onChangePagination(currentPage + 1)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  useEffect(() => {
+    setPerPage(posts.length / 10)
+    initPagination()
+    setupPagination()
+  }, [posts])
+
   return (
     <div className="max-w-xl mt-8 mx-auto flex flex-col gap-y-8">
       <Search />
-      <Post />
-      <Post />
-      <Post />
+      {showPosts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          body={post.body}
+          userName={post.userName}
+        />
+      ))}
       {/* pagination */}
-      <div className="w-full flex justify-end text-slate-400">
+      <div className="w-full flex justify-end text-slate-400 py-20">
         <div className="flex gap-x-4">
-          <p>prev</p>
-          <p className="border-b-4 border-blue-400 cursor-pointer">1</p>
-          <p>2</p>
-          <p>3</p>
-          <p>next</p>
+          <p onClick={onPrev} className="cursor-pointer">
+            prev
+          </p>
+          {pagination.map((page, index) => (
+            <div
+              key={index}
+              className={`px-2 cursor-pointer ${
+                page == currentPage ? "border-blue-400 border-b-4" : ""
+              }`}
+              onClick={() => onChangePagination(Number(page))}
+            >
+              <p>{page}</p>
+            </div>
+          ))}
+          <p onClick={onNext} className="cursor-pointer">
+            next
+          </p>
         </div>
       </div>
       {/* pagination */}
